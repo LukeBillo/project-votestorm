241a2b8d715d0fb47b2d6982709faa4d42f0250e
LukeBillo <n0627514@ntu.ac.uk>
2018-11-27 10:34:40 +0000

    Dependency updates

--- project-votestorm/ClientApp/app/components/counter/counter.component.spec.ts
+++ project-votestorm/ClientApp/app/components/counter/counter.component.spec.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
-import { assert } from 'chai';
 import { CounterComponent } from './counter.component';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
