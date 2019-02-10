b69e0b1571060f5a4eda5f112f86dd747eb843f0
Mohamed Dewidar <mkdewidar@gmail.com>
2019-02-10 00:59:32 +0000

    #26 add config entries to specify properties of scheduled deletion

--- project-votestorm/Data/ScheduledPollDeletion.cs
+++ project-votestorm/Data/ScheduledPollDeletion.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ProjectVotestorm.Data.Repositories;
@@ -9,19 +10,28 @@ namespace ProjectVotestorm.Data
 {
     public class ScheduledPollDeletion : IHostedService, IDisposable
     {
-        private IPollRepository _pollRepository;
+        private readonly IPollRepository _pollRepository;
         private Timer _timer;
-        private ILogger<ScheduledPollDeletion> _logger;
+        private readonly ILogger<ScheduledPollDeletion> _logger;
+        private double _timerPeriod;
+        private double _pollMaxAge;
 
-        public ScheduledPollDeletion(IPollRepository pollRepository, ILogger<ScheduledPollDeletion> logger)
+        public ScheduledPollDeletion(IPollRepository pollRepository, ILogger<ScheduledPollDeletion> logger,
+            IConfiguration config)
         {
             _pollRepository = pollRepository;
             _logger = logger;
+
+            _timerPeriod = Convert.ToDouble(config["AutoPollDeletion:timerFrequencyHours"]);
+            _pollMaxAge = Convert.ToDouble(config["AutoPollDeletion:pollMaxAgeDays"]);
+            
+            _logger.LogInformation("Timer frequency has been set to " + _timerPeriod + " hours");
+            _logger.LogInformation("Poll max age has been set to " + _pollMaxAge + " days");
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _timer = new Timer(DeletePolls, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+            _timer = new Timer(DeletePolls, null, TimeSpan.Zero, TimeSpan.FromHours(_timerPeriod));
 
             return Task.CompletedTask;
         }
@@ -29,7 +39,7 @@ namespace ProjectVotestorm.Data
         private void DeletePolls(Object state)
         {
             _logger.LogInformation("Starting the scheduled poll deletion task");
-            _pollRepository.Delete(DateTime.Now.Subtract(TimeSpan.FromDays(1)));
+            _pollRepository.Delete(DateTime.Now.Subtract(TimeSpan.FromDays(_pollMaxAge)));
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
