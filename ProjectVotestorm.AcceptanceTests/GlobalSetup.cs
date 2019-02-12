e470bd9ccdb38f2a8c099c3cd79a7a3b30479db3
LukeBillo <n0627514@ntu.ac.uk>
2019-02-12 18:46:03 +0000

    #36: Start of acceptance test setup

---
+++ ProjectVotestorm.AcceptanceTests/GlobalSetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProjectVotestorm.AcceptanceTests
+{
+    [SetUpFixture]
+    public class GlobalSetup
+    {
+        public static IWebDriver WebDriver { get; private set; }
+        private IWebHost _serverHost;
+
+        [OneTimeSetUp]
+        public async Task ServerAndWebDriverSetup()
+        {
+            _serverHost = Program
+                .CreateWebHostBuilder(new string[0])
+                .Build();
+
+            await _serverHost.StartAsync();
+
+            var chromeDriverService = ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
+            WebDriver = new ChromeDriver(chromeDriverService);
+        }
+
+        [OneTimeTearDown]
+        public async Task Cleanup()
+        {
+            WebDriver.Quit();
+            await _serverHost.StopAsync(TimeSpan.FromSeconds(5));
+        }
+    }
+}
---
+++ ProjectVotestorm.AcceptanceTests/HomeTests/GivenTheHomePageIsAvailable.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ProjectVotestorm.AcceptanceTests.HomeTests
+{
+    [TestFixture]
+    public class GivenTheHomePageIsAvailable
+    {
+        [OneTimeSetUp]
+        public void WhenTheHomePageIsLoaded()
+        {
+            GlobalSetup.WebDriver.Navigate().GoToUrl("https://localhost:5001/");
+        }
+
+        [Test]
+        public void ThenTheGoToPollComponentIsVisible()
+        {
+            Assert.That(true);
+        }
+
+    }
+}
