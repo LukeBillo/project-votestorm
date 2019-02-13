328223aed2b7d8c968ae76e533b71cd0a852efe3
Mohamed Dewidar <mkdewidar@gmail.com>
2019-02-13 02:34:18 +0000

    #20 validate create form essential fields are not empty
    Poll options beyond 2 can be empty, but are ignored from the request
    sent as an alternative to requiring option deletion.

--- project-votestorm/ClientApp/src/app/create-poll/create-poll.component.ts
+++ project-votestorm/ClientApp/src/app/create-poll/create-poll.component.ts
@@ -5,6 +5,7 @@ import { PollType } from '../models/poll-type.enum';
 import { PollService } from '../services/poll.service';
 import { Router } from '@angular/router';
 import { IdentityService } from '../services/identity.service';
+import {notEmptyValidator} from "../not-empty.validator";
 
 @Component({
   selector: 'create-poll',
@@ -12,8 +13,10 @@ import { IdentityService } from '../services/identity.service';
   styleUrls: ['./create-poll.component.scss'],
 })
 export class CreatePollComponent {
+  essentialValidators = [Validators.required, Validators.maxLength(256), notEmptyValidator];
+
   pollForm = this.formBuilder.group({
-    prompt: ['', Validators.required]
+    prompt: new FormControl('', this.essentialValidators)
   });
   options: Array<number> = [0, 1];
   created = false;
@@ -22,13 +25,16 @@ export class CreatePollComponent {
   constructor(private formBuilder: FormBuilder,
      private pollService: PollService, private router: Router,
      private identityService: IdentityService) {
-    this.pollForm.addControl('option0', new FormControl(''));
-    this.pollForm.addControl('option1', new FormControl(''));
+    this.pollForm.addControl('option0',
+      new FormControl('', this.essentialValidators));
+    this.pollForm.addControl('option1',
+      new FormControl('', this.essentialValidators));
   }
 
   createNewOption() {
     const optionControlName = `option${this.options.length}`;
-    this.pollForm.addControl(optionControlName, new FormControl());
+    this.pollForm.addControl(optionControlName,
+      new FormControl('', [Validators.maxLength(256)]));
     this.options.push(this.options.length);
   }
 
@@ -54,7 +60,9 @@ export class CreatePollComponent {
     const adminIdentity: string = this.identityService.get();
 
     for (let i = 0; i < this.options.length; i++) {
-      options.push(form[`option${i}`].value);
+      if (form[`option${i}`].value.length) {
+        options.push(form[`option${i}`].value);
+      }
     }
 
     return new Poll(prompt, options, pollType, adminIdentity);
