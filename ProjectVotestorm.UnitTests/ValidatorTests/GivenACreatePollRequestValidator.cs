6c65b4eee6d8c3f87a10aa4a4e1a6baa69f04704
LukeBillo <n0627514@ntu.ac.uk>
2019-02-14 21:21:08 +0000

    Further testing for ResultsController and ValidatorTests

---
+++ ProjectVotestorm.UnitTests/ValidatorTests/GivenACreatePollRequestValidator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using ProjectVotestorm.Data.Validators;
+
+namespace ProjectVotestorm.UnitTests.ValidatorTests
+{
+    [TestFixture]
+    public class GivenACreatePollRequestValidator
+    {
+        private CreatePollRequestValidator _validator;
+
+        [OneTimeSetUp]
+        public void WhenACreatePollRequestValidatorIsCreated()
+        {
+            _validator = new CreatePollRequestValidator();
+        }
+
+        [Test]
+        public void ThenAnyStringPromptIsAccepted()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(request => request.Prompt, "I am a prompt");
+        }
+
+        [Test]
+        public void ThenAnyOptionsListWithLengthBetweenTwoAndFiveIsAccepted()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(request => request.Options, new List<string> { "1", "2" });
+            _validator.ShouldNotHaveValidationErrorFor(request => request.Options, new List<string> { "1", "2", "3","4", "5" });
+        }
+
+        [Test]
+        public void ThenAnyStringAdminIdentityIsAccepted()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(request => request.AdminIdentity, Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        public void ThenAnEmptyStringPromptIsNotAccepted()
+        {
+            _validator.ShouldHaveValidationErrorFor(request => request.Prompt, "");
+        }
+
+        [Test]
+        public void ThenAnIncorrectSizeListIsNotAccepted()
+        {
+            _validator.ShouldHaveValidationErrorFor(request => request.Options, new List<string>());
+            _validator.ShouldHaveValidationErrorFor(request => request.Options, new List<string> { "1" });
+            _validator.ShouldHaveValidationErrorFor(request => request.Options, new List<string> { "1", "2", "3", "4", "5", "6" });
+        }
+
+        [Test]
+        public void ThenAnEmptyOptionIsNotAccepted()
+        {
+            _validator.ShouldHaveValidationErrorFor(request => request.Options, new List<string> { "", "" });
+            _validator.ShouldHaveValidationErrorFor(request => request.Options, new List<string> { "1", "", "" });
+        }
+
+        [Test]
+        public void ThenAnEmptyStringAdminIdentityIsNotAccepted()
+        {
+            _validator.ShouldHaveValidationErrorFor(request => request.AdminIdentity, "");
+            _validator.ShouldHaveValidationErrorFor(request => request.AdminIdentity, null as string);
+        }
+    }
+}
---
+++ ProjectVotestorm.UnitTests/ValidatorTests/GivenASetPollStateRequestValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using ProjectVotestorm.Data.Validators;
+
+namespace ProjectVotestorm.UnitTests.ValidatorTests
+{
+    [TestFixture]
+    public class GivenASetPollStateRequestValidator
+    {
+        private SetPollStateRequestValidator _validator;
+
+        [OneTimeSetUp]
+        public void WhenTheSetPollStateRequestValidatorIsCreated()
+        {
+            _validator = new SetPollStateRequestValidator();
+        }
+
+        [Test]
+        public void ThenAnyStringAdminIdentityIsAccepted()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(request => request.AdminIdentity, Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        public void ThenAnEmptyStringAdminIdentityIsNotAccepted()
+        {
+            _validator.ShouldHaveValidationErrorFor(request => request.AdminIdentity, "");
+            _validator.ShouldHaveValidationErrorFor(request => request.AdminIdentity, null as string);
+        }
+
+        [Test]
+        public void ThenTheIsActiveFieldIsNotChecked()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(request => request.IsActive, true);
+            _validator.ShouldNotHaveValidationErrorFor(request => request.IsActive, false);
+            _validator.ShouldNotHaveValidationErrorFor(request => request.IsActive, default(bool));
+        }
+    }
+}
